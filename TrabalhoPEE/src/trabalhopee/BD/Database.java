203430271f4e6ef1e485ffcae76ada542ec3d1ef
Cleyton de Castro <cleytoncm@gmail.com>
2016-06-24 15:20:44 -0300

    Parcial das conexões com os banco de dados!

---
+++ TrabalhoPEE/src/trabalhopee/BD/Database.java
@@ -0,0 +1,23 @@
+package trabalhopee.BD;
+
+/**
+ *
+ * @author Cleyton
+ */
+import java.sql.ResultSet;
+
+public interface Database {
+
+	public boolean connect();
+	
+	public ResultSet query(String sql);
+	
+	public boolean insert(String sql);
+	
+	public boolean update(String sql);
+	
+	public boolean delete(String sql);
+
+	public boolean disconnect();
+	
+}
