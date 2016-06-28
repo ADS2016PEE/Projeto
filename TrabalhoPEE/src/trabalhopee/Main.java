b0397cbebd90b5e09f1da80a8b6ed75cefb47d47
Cleyton de Castro <cleytoncm@gmail.com>
2016-06-28 20:08:34 -0300

    Completo.

--- TrabalhoPEE/src/trabalhopee/Main.java
+++ TrabalhoPEE/src/trabalhopee/Main.java
@@ -1,15 +1,44 @@
 package trabalhopee;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    private static long tempo[][] = new long[30][4];
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Iniciando análise...");
+        int i = 0;
         Produtos produto = new Produtos();
-//        System.out.println("Tempo do MySQL: " + produto.inserirMySQL());
-//        System.out.println("Tempo do PostgreSQL: " + produto.inserirPostGreSQL());
-//        System.out.println("Tempo do Neo4J: " + produto.inserirNeo4J());
-        System.out.println("Tempo do SQL Server: " + produto.inserirSQLServer());
+//        for (int i = 0; i < 30; i++) {
+            tempo[i][0] = produto.inserirMySQL();
+            tempo[i][1] = produto.inserirPostGreSQL();
+            tempo[i][2] = produto.inserirNeo4J();
+            tempo[i][3] = produto.inserirSQLServer();
+//        }
         
+        gerarAnalise();
+        
+        System.out.println("Termino análise...!");
     }
-    
+
+    private static void gerarAnalise() throws IOException {
+        FileWriter arq = new FileWriter("analise.csv");
+        PrintWriter gravarArq = new PrintWriter(arq);
+
+        gravarArq.println("Execução;MySQL;PostGreSQL;Neo4J;SQL Server");
+        
+        for (int i = 0; i < 30; i++) {
+            gravarArq.println((i + 1) + ";" + tempo[i][0] + ";" + tempo[i][1] + ";" + tempo[i][2] + ";"
+                    + +tempo[i][3] + ";");
+        }
+
+        arq.close();
+
+        System.out.println("Arquivo analise.csv, criado com sucesso.");
+        System.out.println("");
+        System.out.println("");
+    }
 }
